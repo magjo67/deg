d3a17e47943949fbc4a738f3dd69a5c0c91f7734
magjo67 <magjo67@gmail.com>
2023-05-10 16:10:02 +0200

    Hydrering surdeg

--- deg/ingredienser.py
+++ deg/ingredienser.py
@@ -1,9 +1,8 @@
 # Ingredienser
 
 class Ingredienser:
-    def __init__(self, kort, long, vikt, pris, procent):
+    def __init__(self, kort, long, vikt, pris):
         self.kort = kort
         self.long = long
         self.vikt = vikt
         self.pris = pris
-        self.procent = procent
