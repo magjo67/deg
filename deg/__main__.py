880a2a435224cf37eab91b19b59de65ed59d526e
magjo67 <magjo67@gmail.com>
2023-05-24 09:25:47 +0200

    Install via pyproject.toml

---
+++ deg/__main__.py
@@ -0,0 +1,4 @@
+import deg
+
+if __name__ == "__main__":
+    deg.do_deg()
--- deg/ingredienser.py
+++
@@ -1,8 +0,0 @@
-# Ingredienser
-
-class Ingredienser:
-    def __init__(self, kort, long, vikt, pris):
-        self.kort = kort
-        self.long = long
-        self.vikt = vikt
-        self.pris = pris
